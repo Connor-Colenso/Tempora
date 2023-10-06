376018632511b1c788f2a18a4bd906f7257ca35d
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-10-06 02:11:46 +0100

    Clean up

--- src/main/java/com/myname/mymodid/Loggers/BlockBreakLogger.java
+++ src/main/java/com/myname/mymodid/Loggers/BlockBreakLogger.java
@@ -56,21 +56,6 @@ public class BlockBreakLogger {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        } else {
-            try {
-                String sql = "INSERT INTO BlockBreakEvents(playerName, blockType, metadata, x, y, z, dimensionID) VALUES(?, ?, ?, ?, ?, ?, ?)";
-                PreparedStatement pstmt = conn.prepareStatement(sql);
-                pstmt.setString(1, event.getPlayer().getDisplayName());
-                pstmt.setString(2, event.block.getUnlocalizedName());  // This will get the block's unlocalized name, adjust as needed.
-                pstmt.setInt(3, event.blockMetadata);
-                pstmt.setInt(4, event.x);
-                pstmt.setInt(5, event.y);
-                pstmt.setInt(6, event.z);
-                //pstmt.setInt(7, event.getPlayer().worldObj.provider.dimensionId);  // This fetches the dimension ID.
-                pstmt.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
