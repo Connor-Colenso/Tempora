ac80b5d7b16ffa1674847b36f87300b509a6c321
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-10-10 20:38:58 +0100

    Store all dim changes, working

--- src/main/java/com/myname/mymodid/Loggers/PlayerMovementLogger.java
+++ src/main/java/com/myname/mymodid/Loggers/PlayerMovementLogger.java
@@ -4,6 +4,7 @@ import com.myname.mymodid.Commands.TrackPlayer.TrackPlayerUpdater;
 import com.myname.mymodid.TemporaUtils;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -59,28 +60,41 @@ public class PlayerMovementLogger extends GenericLogger {
         // Events are only logged server side every 5 seconds at the start of a tick.
         if (isClientSide()) return;
         if (event.phase != TickEvent.Phase.START) return;
+        if (!(event.player instanceof EntityPlayerMP player)) return;
 
-        // Trigger this tracking every 5 seconds. Todo make this timer changeable in the config.
-//        if (FMLCommonHandler.instance()
-//            .getMinecraftServerInstance()
-//            .getTickCounter() % 100 != 0) return;
+        // Trigger this tracking every 5 seconds.
+        // Todo make this timer changeable in the config.
+        if (FMLCommonHandler.instance()
+            .getMinecraftServerInstance()
+            .getTickCounter() % 100 != 0) return;
 
-        // Now we do actual processing of this event.
-        if (event.player instanceof EntityPlayerMP) {
-            try {
-                final String sql = "INSERT INTO PlayerMovementEvents(playerName, x, y, z, dimensionID, timestamp) VALUES(?, ?, ?, ?, ?, ?)";
-                final PreparedStatement pstmt = conn.prepareStatement(sql);
-                pstmt.setString(1, event.player.getDisplayName());
-                pstmt.setDouble(2, event.player.posX);
-                pstmt.setDouble(3, event.player.posY);
-                pstmt.setDouble(4, event.player.posZ);
-                pstmt.setInt(5, event.player.worldObj.provider.dimensionId);
-                pstmt.setLong(6, System.currentTimeMillis());  // Set the timestamp with millisecond precision
-                pstmt.executeUpdate();
+        saveData(player);
+    }
 
-            } catch (final SQLException e) {
-                e.printStackTrace();
-            }
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if (isClientSide()) return;
+        if (!(event.player instanceof EntityPlayerMP player)) return;
+
+        saveData(player);
+    }
+
+    private void saveData(final EntityPlayerMP player) {
+        try {
+            final String sql = "INSERT INTO PlayerMovementEvents(playerName, x, y, z, dimensionID, timestamp) VALUES(?, ?, ?, ?, ?, ?)";
+            final PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, player.getDisplayName());
+            pstmt.setDouble(2, player.posX);
+            pstmt.setDouble(3, player.posY);
+            pstmt.setDouble(4, player.posZ);
+            pstmt.setInt(5, player.worldObj.provider.dimensionId);
+            pstmt.setLong(6, System.currentTimeMillis());  // Set the timestamp with millisecond precision
+            pstmt.executeUpdate();
+
+        } catch (final SQLException e) {
+            e.printStackTrace();
         }
     }
+
 }
