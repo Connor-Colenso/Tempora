440772739ac52c7546ed6abccb86cf7a08df756d
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2024-04-14 15:10:58 +0100

    Update bs and some other work to try get mixins working

--- src/main/java/com/colen/tempora/mixins/NetHandlerPlayServerMixin.java
+++ src/main/java/com/colen/tempora/mixins/NetHandlerPlayServerMixin.java
@@ -3,12 +3,12 @@ package com.colen.tempora.mixins;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.client.C0EPacketClickWindow;
 
-@Mixin(targets = "net.minecraft.entity.player.EntityPlayerMP") // Specify the class to target
-public class NetHandlerPlayServerMixin {
-
-    @Inject(method = "processClickWindow(Lnet/minecraft/network/play/client/C0EPacketClickWindow;)V", at = @At("HEAD"))
-    private void injectProcessClickWindow(C0EPacketClickWindow packetIn, CallbackInfo ci) {
-        MyUtils.processPacket((EntityPlayerMP)(Object)this, packetIn); // Cast 'this' to the appropriate type
-    }
-
-}
+//@Mixin(targets = "net.minecraft.entity.player.EntityPlayerMP") // Specify the class to target
+//public class NetHandlerPlayServerMixin {
+//
+//    @Inject(method = "processClickWindow(Lnet/minecraft/network/play/client/C0EPacketClickWindow;)V", at = @At("HEAD"))
+//    private void injectProcessClickWindow(C0EPacketClickWindow packetIn, CallbackInfo ci) {
+//        MyUtils.processPacket((EntityPlayerMP)(Object)this, packetIn); // Cast 'this' to the appropriate type
+//    }
+//
+//}
--- src/main/java/com/colen/tempora/mixins/TemporaLoadingPlugin.java
+++ src/main/java/com/colen/tempora/mixins/TemporaLoadingPlugin.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
-public final class TemporaLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public final class TemporaLoadingPlugin implements IFMLLoadingPlugin {
 
     @Override
     public String[] getASMTransformerClass() {
@@ -33,15 +33,15 @@ public final class TemporaLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixi
     @Override
     public void injectData(Map<String, Object> data) {}
 
-    @Override
-    public String getMixinConfig() {
-        return "mixins.tempora.early.json";
-    }
-
-    @Override
-    public List<String> getMixins(Set<String> loadedCoreMods) {
-        final List<String> mixins = new ArrayList<>();
-        mixins.add("NetHandlerPlayServerMixin");
-        return mixins;
-    }
+//    @Override
+//    public String getMixinConfig() {
+//        return "mixins.tempora.early.json";
+//    }
+//
+//    @Override
+//    public List<String> getMixins(Set<String> loadedCoreMods) {
+//        final List<String> mixins = new ArrayList<>();
+//        mixins.add("NetHandlerPlayServerMixin");
+//        return mixins;
+//    }
 }
