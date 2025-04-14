2035746f0b5a115c259196c4af8c1ac7efa463f2
Connor Colenso <colen@CONNORSPC>
2025-04-14 01:05:58 +0100

    Prevent crash with NPE

--- src/main/java/com/colen/tempora/utils/ItemUtils.java
+++ src/main/java/com/colen/tempora/utils/ItemUtils.java
@@ -6,7 +6,13 @@ import net.minecraft.item.ItemStack;
 public class ItemUtils {
 
     public static String getNameOfItemStack(int id, int meta) {
-        ItemStack itemStack = new ItemStack(Item.getItemById(id), 1, meta);
+        Item item = Item.getItemById(id);
+        if (item == null) {
+            String errorItemName = "[ERROR UNKNOWN ITEM (" + id + ":" + meta + ")]";
+            System.err.println(errorItemName);
+            return errorItemName;
+        }
+        ItemStack itemStack = new ItemStack(item, 1, meta);
         return itemStack.getDisplayName();
     }
 }
