cff3edc0ce6f5a4d0d0d75a59a06914ad84028af
Connor Colenso <colen@CONNORSPC>
2025-07-07 02:18:01 +0100

    Scaling

--- src/main/java/com/colen/tempora/rendering/LastWorldEventRenderer.java
+++ src/main/java/com/colen/tempora/rendering/LastWorldEventRenderer.java
@@ -13,38 +13,45 @@ import org.lwjgl.opengl.GL11;
 @SideOnly(Side.CLIENT)
 public final class LastWorldEventRenderer {
 
+    private static final double SCALE_FACTOR = 0.9;   // 90 %
+
     @SubscribeEvent
     public void onRender(RenderWorldLastEvent e) {
         if (PacketDetectedInfo.CLIENT_POS.isEmpty()) return;
 
-        Tessellator tessellator = Tessellator.instance;
+        Tessellator tes = Tessellator.instance;
         Minecraft mc = Minecraft.getMinecraft();
 
-        double playerX = mc.thePlayer.lastTickPosX + (mc.thePlayer.posX - mc.thePlayer.lastTickPosX) * e.partialTicks;
-        double playerY = mc.thePlayer.lastTickPosY + (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) * e.partialTicks;
-        double playerZ = mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ) * e.partialTicks;
+        double px = mc.thePlayer.lastTickPosX + (mc.thePlayer.posX - mc.thePlayer.lastTickPosX) * e.partialTicks;
+        double py = mc.thePlayer.lastTickPosY + (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) * e.partialTicks;
+        double pz = mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ) * e.partialTicks;
 
         int curDim = mc.thePlayer.dimension;
 
         GL11.glPushMatrix();
+        GL11.glTranslated(-px, -py, -pz);
 
-        // Translate world so player is at the origin (view transform)
-        GL11.glTranslated(-playerX, -playerY, -playerZ);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(1f, 1f, 1f, 0.5f);
 
         for (PacketDetectedInfo.Pos pos : PacketDetectedInfo.CLIENT_POS) {
             if (pos.dim != curDim) continue;
 
             GL11.glPushMatrix();
             GL11.glTranslated(pos.x + 0.5, pos.y + 0.5, pos.z + 0.5);
+            GL11.glScaled(SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR);
 
-            tessellator.startDrawingQuads();
+            tes.startDrawingQuads();
             RenderUtils.addRenderedBlockInWorld(Blocks.cobblestone, 0, 0, 0, 0);
-            tessellator.draw();
+            tes.draw();
 
             GL11.glPopMatrix();
         }
 
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glPopMatrix();
     }
-
 }
