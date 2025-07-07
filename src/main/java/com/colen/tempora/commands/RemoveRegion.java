9e85b2356f81501bac134c33d80830e1344d0795
Connor Colenso <colen@CONNORSPC>
2025-07-07 22:09:19 +0100

    Complete RemoveRegion localisation

--- src/main/java/com/colen/tempora/commands/RemoveRegion.java
+++ src/main/java/com/colen/tempora/commands/RemoveRegion.java
@@ -1,15 +1,14 @@
 package com.colen.tempora.commands;
 
-import java.util.Arrays;
-import java.util.List;
-
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
 import com.colen.tempora.logging.loggers.block_change.RegionRegistry;
@@ -27,6 +26,7 @@ public class RemoveRegion extends CommandBase {
         return "removeregion";
     }
 
+
     @Override
     public String getCommandUsage(ICommandSender sender) {
         return "/removeregion";
@@ -39,24 +39,38 @@ public class RemoveRegion extends CommandBase {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-        if (args.length != 0) throw new WrongUsageException(getCommandUsage(sender));
+        // Syntax check
+        if (args.length != 0) {
+            throw new WrongUsageException(getCommandUsage(sender));
+        }
 
-        // Only players have a sensible position
-        if (!(sender instanceof EntityPlayer)) throw new CommandException("Only a player can run /removeregion.");
+        // removeregion only makes sense for a player, because it users a players coords
+        if (!(sender instanceof EntityPlayer)) {
+            // This could go to an unlocalised client, like a terminal, so don't localise.
+            throw new CommandException("Only players in world may use this command.");
+        }
 
         ChunkCoordinates pos = sender.getPlayerCoordinates();
-        World world = sender.getEntityWorld();
-        int dim = world.provider.dimensionId;
+        World              world = sender.getEntityWorld();
+        int                dim = world.provider.dimensionId;
 
         int removed = RegionRegistry.get(world)
             .removeRegionsContainingBlock(dim, pos.posX, pos.posY, pos.posZ);
 
+        ChatComponentTranslation msg;
         if (removed > 0) {
-            sender.addChatMessage(
-                new ChatComponentText(
-                    String.format("§aRemoved %d region%s at your position.", removed, removed == 1 ? "" : "s")));
+            // Plural, or not.
+            String key = removed == 1
+                ? "command.tempora.removeregion.removed.single"
+                : "command.tempora.removeregion.removed.plural";
+
+            msg = new ChatComponentTranslation(key, removed);
+            msg.getChatStyle().setColor(EnumChatFormatting.GREEN);
+            sender.addChatMessage(msg);
         } else {
-            sender.addChatMessage(new ChatComponentText("§eNo region found at your position."));
+            msg = new ChatComponentTranslation("command.tempora.removeregion.no_region");
+            msg.getChatStyle().setColor(EnumChatFormatting.RED);
         }
+        sender.addChatMessage(msg);
     }
 }
