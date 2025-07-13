29b8e38671e95aa9e898d59a60c7c34e5fcb7416
Connor Colenso <colen@CONNORSPC>
2025-07-13 01:48:23 +0100

    Render regions refactor

--- src/main/java/com/colen/tempora/rendering/RenderRegionsInWorld.java
+++ src/main/java/com/colen/tempora/rendering/RenderRegionsInWorld.java
@@ -22,16 +22,28 @@ public final class RenderRegionsInWorld {
 
     @SubscribeEvent
     public void onRender(RenderWorldLastEvent e) {
-        if (PacketShowRegionInWorld.CLIENT_REGIONS.isEmpty()) return;
-
         Minecraft mc = Minecraft.getMinecraft();
         double px = mc.thePlayer.lastTickPosX + (mc.thePlayer.posX - mc.thePlayer.lastTickPosX) * e.partialTicks;
         double py = mc.thePlayer.lastTickPosY + (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) * e.partialTicks;
         double pz = mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ) * e.partialTicks;
         int curDim = mc.thePlayer.dimension;
 
-        GL11.glPushMatrix();
-        GL11.glTranslated(-px, -py, -pz);
+        for (IntRegion r : PacketShowRegionInWorld.CLIENT_REGIONS) {
+            if (r.dim != curDim) continue;
+            GL11.glPushMatrix();
+            GL11.glTranslated(-px, -py, -pz);
+
+            renderRegion(r.minX, r.minY, r.minZ, r.maxX + 1, r.maxY + 1, r.maxZ + 1);
+
+            GL11.glPopMatrix();
+        }
+
+        double expiryCutoff = System.currentTimeMillis() - SECONDS_RENDERING_DURATION * 1000L;
+        PacketShowRegionInWorld.CLIENT_REGIONS.removeIf(intRegion -> intRegion.posPrintTime < expiryCutoff);
+    }
+
+    private void renderRegion(double startX, double startY, double startZ,
+                              double endX, double endY, double endZ) {
 
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
@@ -41,20 +53,12 @@ public final class RenderRegionsInWorld {
         GL11.glLineWidth(2F);
         GL11.glColor4f(1F, 0F, 0F, 0.7F);
 
-        for (IntRegion r : PacketShowRegionInWorld.CLIENT_REGIONS) {
-            if (r.dim != curDim) continue;
-
-            AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(r.minX, r.minY, r.minZ, r.maxX + 1, r.maxY + 1, r.maxZ + 1);
-
-            RenderGlobal.drawOutlinedBoundingBox(bb, 0xFFFFFFFF); // just lines
-        }
+        AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(startX, startY, startZ, endX, endY, endZ);
+        RenderGlobal.drawOutlinedBoundingBox(bb, 0xFFFFFFFF);
 
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glPopMatrix();
-
-        double expiryCutoff = System.currentTimeMillis() - SECONDS_RENDERING_DURATION * 1000L;
-        PacketShowRegionInWorld.CLIENT_REGIONS.removeIf(intRegion -> intRegion.posPrintTime < expiryCutoff);
     }
+
 }
