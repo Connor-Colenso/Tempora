39db98a7b96a0a6d53254d1c9c076886a4a0a562
Connor Colenso <colen@CONNORSPC>
2025-11-06 16:47:06 +0000

    Update comment

--- src/main/java/com/colen/tempora/commands/CommandConstants.java
+++ src/main/java/com/colen/tempora/commands/CommandConstants.java
@@ -2,5 +2,6 @@ package com.colen.tempora.commands;
 
 public class CommandConstants {
 
+    // Note this is not localised, because it may be called from terminal, which has no localisation functionality.
     public static final String ONLY_IN_GAME = "This command may only be executed by a player in-game.";
 }
