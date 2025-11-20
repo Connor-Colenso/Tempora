6d8302e0833cf0959b1eb60cd7159d1a4e940b4d
Connor Colenso <connorc@blackmagicdesign.com>
2025-11-20 16:27:48 +0000

    Wand changes + localisation

--- src/main/java/com/colen/tempora/items/TemporaWand.java
+++ src/main/java/com/colen/tempora/items/TemporaWand.java
@@ -1,15 +1,21 @@
 package com.colen.tempora.items;
 
+import static com.colen.tempora.loggers.generic.GenericQueueElement.generateTeleportChatComponent;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.Facing;
 import net.minecraft.world.World;
 
 import com.colen.tempora.TemporaUtils;
 import com.colen.tempora.loggers.generic.GenericPositionalLogger;
+import com.colen.tempora.loggers.generic.GenericQueueElement;
 import com.colen.tempora.utils.PlayerUtils;
+
+import cpw.mods.fml.common.FMLLog;
 
 public class TemporaWand extends Item {
 
@@ -22,26 +28,62 @@ public class TemporaWand extends Item {
 
     @Override
     public boolean onBlockStartBreak(ItemStack stack, int x, int y, int z, EntityPlayer player) {
+        if (TemporaUtils.isServerSide()) {
+            checkSpot(player, x, y, z);
+        }
+
+        // On the client, force a visual refresh so the block doesn't appear broken
+        if (TemporaUtils.isServerSide()) {
+            player.worldObj.markBlockForUpdate(x, y, z);
+        }
+
+        // Cancel the break on both sides
         return true;
     }
 
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
         float hitX, float hitY, float hitZ) {
-        if (TemporaUtils.isClientSide()) return false;
+        if (TemporaUtils.isClientSide()) return true;
 
+        int px = x + Facing.offsetsXForSide[side];
+        int py = y + Facing.offsetsYForSide[side];
+        int pz = z + Facing.offsetsZForSide[side];
+
+        checkSpot(player, px, py, pz);
+
+        // Todo send client render packet to highlight selected coordinate.
+
+        return true;
+    }
+
+    private static void checkSpot(EntityPlayer player, int x, int y, int z) {
         if (!PlayerUtils.isPlayerOp(player)) {
             PlayerUtils.sendMessageToOps("player.tempora.wand.unauthorised", player.getDisplayName());
+            FMLLog.warning(
+                "[TemporaWand] Unauthorised use attempt by player '%s' at (%d, %d, %d) in dimension %d.",
+                player.getDisplayName(),
+                x,
+                y,
+                z,
+                player.dimension);
         } else {
-            for (GenericPositionalLogger<?> logger : GenericPositionalLogger.getLoggerList()) {
-                int px = x + Facing.offsetsXForSide[side];
-                int py = y + Facing.offsetsYForSide[side];
-                int pz = z + Facing.offsetsZForSide[side];
+            player.addChatMessage(
+                new ChatComponentTranslation(
+                    "msg.tempora.wand.checking.pos",
+                    generateTeleportChatComponent(
+                        x,
+                        y,
+                        z,
+                        player.dimension,
+                        player.getDisplayName(),
+                        GenericQueueElement.CoordFormat.INT),
+                    player.dimension)
+            );
 
-                GenericPositionalLogger.queryEventsAtPosAndTime(player, px, py, pz, -1, logger.getSQLTableName());
+            for (GenericPositionalLogger<?> logger : GenericPositionalLogger.getLoggerList()) {
+                GenericPositionalLogger.queryEventsAtPosAndTime(player, x, y, z, -1, logger.getSQLTableName());
             }
         }
-
-        return false;
     }
 }
